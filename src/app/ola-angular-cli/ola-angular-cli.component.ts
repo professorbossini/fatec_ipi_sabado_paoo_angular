77b2461a27c23e9621dbd85722449c13b2cd8597
Rodrigo Bossini <professorbossini@gmail.com>
2020-07-23 21:22:11 -0300

    terminamos o pdf 01

---
+++ src/app/ola-angular-cli/ola-angular-cli.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ola-angular-cli',
+  templateUrl: './ola-angular-cli.component.html',
+  styleUrls: ['./ola-angular-cli.component.css']
+})
+export class OlaAngularCliComponent {
+
+
+
+}
